7c23750dea069682e8934029a2cfff85547a0810
mailagent0001 <mailagent0001@gmail.com>
2026-07-05 12:01:34 +0900

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://a-zent.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
