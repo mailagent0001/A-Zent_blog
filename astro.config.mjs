2b5059a9a4ab7baeb60883b91039bf36a8b0926d
mailagent0001 <mailagent0001@gmail.com>
2026-07-11 19:28:29 +0900

    Add redirects to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://a-zent.com',
+	redirects: {
+		'/': '/blog',
+	},
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
